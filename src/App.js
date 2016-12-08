f2bf517d204e44f9776299793b91691828a78e1e
Kevin Connor <kevinpaulconnor@gmail.com>
2016-12-07 20:30:11 -0800

    stub components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+// probably a better way to do this
+import unm from './logos/unm.png'
 import './App.css';
 
 class App extends Component {
@@ -7,15 +9,56 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={unm} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Scoreline />
       </div>
     );
   }
+}
+
+class Scoreline extends Component {
+		render() {
+			return (
+			<div className="Scoreline"> 
+				{/*<User/>
+				<Matchup/>
+				<User/>*/}
+			</div>
+		);
+	}
+}
+
+class User extends Component {
+
+}
+
+class Matchup extends Component {
+
+}
+
+class Team extends Component {
+
+}
+
+class Game extends Component {
+	
+}
+
+class Standings extends Component {
+
+}
+
+class UserPicks extends Component {
+
+}
+
+class UserTotal extends Component {
+
 }
 
 export default App;
