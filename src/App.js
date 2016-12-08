fe71b713bf6b9bf1da96e5f0a8c25fe0dc56a4ff
Kevin Connor <kevinpaulconnor@gmail.com>
2016-12-08 15:45:28 -0800

    teams and games stubbing layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 // probably a better way to import these images
-import unm from './logos/unm.png'
-import utsa from './logos/utsa.png'
+import logoUnm from './logos/unm.png'
+import logoUtsa from './logos/utsa.png'
 import schexImage from './users/schex.jpg'
 import danImage from './users/dan.jpg'
 import patImage from './users/pat.jpg'
@@ -22,17 +22,31 @@ function createUser(image) {
 	});
 }
 
+var TEAMS = {
+	newmexico: createTeam('New Mexico', logoUnm),
+	utsa: createTeam("Texas-San Antonio", logoUtsa)
+}
+
+function createTeam(name, logo){
+	return({
+		name: name,
+		logo: logo
+	});
+
+}
+
 var GAMES = [
-	createBowlGame("New Mexico Bowl", "University Stadium", "Dec 17, 2016 11:00", "Albuquerque, NM",
-		"New Mexico", "Texas-San Antonio", USERS.kevin, USERS.schex)
+	createBowlGame("New Mexico Bowl", "University Stadium", "Albuquerque, NM", "Dec 17, 2016 11:00", "ESPN",
+		TEAMS.newmexico, TEAMS.utsa, USERS.kevin, USERS.schex)
 ];
 
-function createBowlGame(name, stadium, location, datestring, team1, team2, team1User, team2User) {
+function createBowlGame(name, stadium, location, datestring, tv, team1, team2, team1User, team2User) {
 	return ({
 		name: name,
 		stadium: stadium,
 		location: location,
 		date: new Date(datestring),
+		tv: tv,
 		team1: team1,
 		team2, team2,
 		team1User: team1User,
@@ -45,7 +59,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={unm} className="App-logo" alt="logo" />
+          <img src={logoUnm} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
@@ -62,7 +76,7 @@ class Scoreline extends Component {
 			return (
 			<div className="Scoreline"> 
 				<Userline user={this.props.games[0].team1User}/>
-				<Matchup/>
+				<Matchup game={this.props.games[0]}/>
 				<Userline user={this.props.games[0].team2User}/>
 			</div>
 		);
@@ -80,16 +94,47 @@ class Userline extends Component {
 class Matchup extends Component {
 	render() {
 		return (
-			<div className="Matchup-container">matchup</div>
+			<div className="Matchup-container">
+			{<Team team={this.props.game.team1}/>}
+			<Game game={this.props.game} />
+			{/*<Team />*/}
+			</div>
 		);
 	}
 }
 
 class Team extends Component {
-
+	render() {
+		return (
+			<div className="Team-container">
+			<span className="Team-score">-</span>
+			<span className="Team-name">{this.props.team.name}</span>
+			<img src={this.props.team.logo} className="Team-logo"/>
+			</div>
+		);
+	}
 }
 
 class Game extends Component {
+		
+	render() {
+		// doesn't seem like this should really be in the render
+		function dateFormat(date) {
+			var options = { weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+			return date.toLocaleString('en-US', options)
+		}
+	
+		var formattedDate = dateFormat(this.props.game.date);
+		return (
+			<div className="Game-container">
+			<div className="Game-bowlname">{this.props.game.name}</div>
+			<div className="Game-stadium">{this.props.game.stadium}</div>			
+			<div className="Game-location">{this.props.game.location}</div>				
+			<div className="Game-tv">{this.props.game.tv}</div>
+			<div className="Game-date">{formattedDate}</div>
+			</div>
+		);
+	}
 	
 }
 
